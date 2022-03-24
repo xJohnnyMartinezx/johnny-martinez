64a272285ea57fcab3cf6b5f678a020f7cfb65bb
Johnny Martinez <mrtnz.jn@gmail.com>
2022-03-24 16:14:55 -0500

    uploaded new images and modified index.html and index.css.

---
+++ js/index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+
+$(document).ready(function (){
+    // alert("ready")
+
+//     function reveal() {
+//         var reveals = document.querySelectorAll(".reveal");
+//         for (var i = 0; i < reveals.length; i++) {
+//             var windowHeight = window.innerHeight;
+//             console.log("winHeigth" + windowHeight)
+//             var elementTop = reveals[i].getBoundingClientRect().top;
+//             console.log("elemTom" + elementTop)
+//             var elementVisible = 200;
+//             if (elementTop < windowHeight - elementVisible) {
+//             //     reveals[i].classList.add("active");
+//             // } else {
+//             //     reveals[i].classList.remove("active");
+//                 var photo = document.querySelectorAll(".profile-photo")
+//                     photo.classList.add("active");
+//             } else {
+//                 var photo = document.querySelectorAll(".profile-photo")
+//                    photo.classList.remove("active");
+//             }
+//
+//         }
+//     }
+//
+//     window.addEventListener("scroll", reveal);
+//
+// // To check the scroll position on page load
+//     reveal();
+//
+//
+
+
+})
