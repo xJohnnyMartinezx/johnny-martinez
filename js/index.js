855b22f4d69ce48660b88e1cd393f8da719f0615
Johnny Martinez <mrtnz.jn@gmail.com>
2022-03-25 22:10:30 -0500

    modified files

--- js/index.js
+++ js/index.js
@@ -1,37 +1,57 @@
 "use strict";
 
 
-$(document).ready(function (){
-    // alert("ready")
+$(document).ready(function () {
+// alert("test")
 
-//     function reveal() {
-//         var reveals = document.querySelectorAll(".reveal");
-//         for (var i = 0; i < reveals.length; i++) {
-//             var windowHeight = window.innerHeight;
-//             console.log("winHeigth" + windowHeight)
-//             var elementTop = reveals[i].getBoundingClientRect().top;
-//             console.log("elemTom" + elementTop)
-//             var elementVisible = 200;
-//             if (elementTop < windowHeight - elementVisible) {
-//             //     reveals[i].classList.add("active");
-//             // } else {
-//             //     reveals[i].classList.remove("active");
-//                 var photo = document.querySelectorAll(".profile-photo")
-//                     photo.classList.add("active");
-//             } else {
-//                 var photo = document.querySelectorAll(".profile-photo")
-//                    photo.classList.remove("active");
-//             }
-//
-//         }
-//     }
-//
-//     window.addEventListener("scroll", reveal);
-//
-// // To check the scroll position on page load
-//     reveal();
-//
-//
+    // window.addEventListener("scroll", () => {
+    //
+    //     console.log("user scrolled")
+    //     const scrolled = window.scrollY;
+    //     const docHeight = document.documentElement.scrollHeight;
+    //     const winHeight = window.innerHeight;
+    //     const sweetSpot = 265
+    //     console.log(scrolled);
+    //
+    //     if (Math.ceil(scrolled) >= sweetSpot) {
+    //         var photo = document.getElementById("profile-photo-nav" )
+    //         photo.classList.remove("reveal")
+    //     } else {
+    //         var photo = document.getElementById("profile-photo-nav")
+    //         photo.classList.add("reveal")
+    //     }
+    // });
+
+    $(document).on("scroll", function () {
+
+        const scrolled = window.scrollY;
+        const sweetSpot = 265
+        // console.log(scrolled);
+
+        if (Math.ceil(scrolled) >= sweetSpot) {
+            var photo = document.getElementById("profile-photo-nav")
+            photo.classList.remove("reveal")
+        } else {
+            var photo = document.getElementById("profile-photo-nav")
+            photo.classList.add("reveal")
+        }
+    })
+
+    $(document).on("scroll", function () {
+
+        const scrolled = window.scrollY;
+        const sweetSpot = 265
+        // console.log(scrolled);
+
+        if (Math.ceil(scrolled) >= sweetSpot) {
+            var name = document.getElementById("navbar-name")
+            name.classList.remove("navbar-brand")
+        } else {
+            var name = document.getElementById("navbar-name")
+            name.classList.add("navbar-brand")
+        }
+
+    })
 
 
 })
