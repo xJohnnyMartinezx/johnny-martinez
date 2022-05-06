352822567be40380e90d04a1a1c1514a4ea46f2c
Johnny Martinez <mrtnz.jn@gmail.com>
2022-05-06 09:01:02 -0500

    added intersection observer to a few more elements.

--- js/index.js
+++ js/index.js
@@ -21,54 +21,79 @@ $(document).ready(function () {
     //         photo.classList.add("reveal")
     //     }
     // });
+    //************************************************
+    // $(document).on("scroll", function () {
+    //
+    //     const scrolled = window.scrollY;
+    //     const sweetSpot = 550        // console.log(scrolled);
+    //
+    //     if (Math.ceil(scrolled) >= sweetSpot) {
+    //         var photo = document.getElementById("profile-photo-nav")
+    //         var name = document.getElementById("reveal-name")
+    //         photo.classList.remove("reveal")
+    //         name.classList.remove("reveal")
+    //     } else {
+    //         var photo = document.getElementById("profile-photo-nav")
+    //         var name = document.getElementById("reveal-name")
+    //         photo.classList.add("reveal")
+    //         name.classList.add("reveal")
+    //     }
+    // })
+    //*******************************************
+    //*************************************************
+    // var toggleBtn = document.getElementById("btn-trigger")
+    // console.log(toggleBtn);
+    //
+    // toggleBtn.addEventListener("click", function () {
+    //
+    //     var name = document.getElementById("image")
+    //         name.classList.toggle("hidden-profile");
+    //
+    //     // if (toggleBtn === true) {
+    //     //     var name = document.getElementById("image")
+    //     //     name.classList.add("hidden-profile");
+    //     //
+    //     // } else {
+    //     //     var name = document.getElementById("image")
+    //     //     name.classList.remove("hidden-profile")
+    //     // }
+    //
+    // })
+    //****************************************************
+    // const smallProfileImg = document.querySelectorAll(".reveal")
+    //
+    // const smallProfileIgmOptions = {
+    //     root: null
+    // };
+    //
+    // const smallProfileImgObserver = new IntersectionObserver(entries => {
+    //     // const smallProfileImg = entries[0]
+    //     entries.forEach(entry => {
+    //         entry.target.classList.toggle("reveal", entry.isIntersecting)
+    //     })
+    // }, smallProfileIgmOptions);
+    //
+    // smallProfileImgObserver.observe(smallProfileImg[0]);
 
-    $(document).on("scroll", function () {
 
-        const scrolled = window.scrollY;
-        const sweetSpot = 550        // console.log(scrolled);
+    //*****************************************************
 
-        if (Math.ceil(scrolled) >= sweetSpot) {
-            var photo = document.getElementById("profile-photo-nav")
-            var name = document.getElementById("reveal-name")
-            photo.classList.remove("reveal")
-            name.classList.remove("reveal")
-        } else {
-            var photo = document.getElementById("profile-photo-nav")
-            var name = document.getElementById("reveal-name")
-            photo.classList.add("reveal")
-            name.classList.add("reveal")
-        }
-    })
-
-    var toggleBtn = document.getElementById("btn-trigger")
-    console.log(toggleBtn);
-
-    toggleBtn.addEventListener("click", function () {
-
-        var name = document.getElementById("image")
-            name.classList.toggle("hidden-profile");
-
-        // if (toggleBtn === true) {
-        //     var name = document.getElementById("image")
-        //     name.classList.add("hidden-profile");
-        //
-        // } else {
-        //     var name = document.getElementById("image")
-        //     name.classList.remove("hidden-profile")
-        // }
-
-    })
-
-    const largeProfileImg = document.querySelectorAll(".hidden-profile")
+    const largeProfileImg = document.querySelectorAll(".intro-line")
 
     const largeProfileImgObserver = new IntersectionObserver(entries => {
         entries.forEach(entry =>{
-            entry.target.classList.toggle("hidden-profile", entry.isIntersecting)
-        }, {
-            threshold: 1,
-        } )
+            if (!entry.isIntersecting){
+                return;
+            }
+            entry.target.classList.toggle("large-photo", entry.isIntersecting)
+        })
         console.log(entries)
-    })
+    }, {
+        root: null,
+        threshold: .50
+        // rootMargin: "350px"
+
+    } );
     largeProfileImgObserver.observe(largeProfileImg[0])
 
     //-----------------------ONSCROLL LINE ANIMATIONS TOGGLE----------------------
